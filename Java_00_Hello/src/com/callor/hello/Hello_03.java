983c259be31ac5138341d6f3f67d0eb668110271
leebin55 <yublee55@gmail.com>
2021-03-16 15:42:00 +0900

    Hello03 추가

---
+++ Java_00_Hello/src/com/callor/hello/Hello_03.java
@@ -0,0 +1,17 @@
+package com.callor.hello;
+
+public class Hello_03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println( 30 + 40 );
+		System.out.println( 30 - 40 );
+		System.out.println( 30 * 40 );
+		System.out.println( 30 / 40 );
+		System.out.println( 30 % 40 );
+		System.out.println( 1 % 2 );
+		System.out.println( 2 % 2 );
+		System.out.println( 3 % 2 );
+	}
+
+}
